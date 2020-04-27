73c89a91e5f4b1134caaad047fc2fc33a4bf5fc4
iAquino47 <aquinoian@icloud.com>
2020-04-27 22:52:10 +0100

    selected dish is highlighted on the list

--- src/js/index.js
+++ src/js/index.js
@@ -66,6 +66,9 @@ const controlRecipe = async () => {
         recipeView.clearRecipe();
         renderLoader(elements.recipe)
 
+        //Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+
         //create recipe object
         state.recipe = new Recipe(id);
 
