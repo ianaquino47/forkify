8e91c046f333cf6ea6282cd3c92382f99a50af7a
iAquino47 <aquinoian@icloud.com>
2020-04-27 22:24:38 +0100

    recipe now showing on UI

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
 import * as searchView from './views/searchView'
+import * as recipeView from './views/recipeView'
 import { elements, renderLoader, clearLoader } from './views/base'
 
 // Global state of the app
@@ -62,6 +63,9 @@ const controlRecipe = async () => {
 
     if (id) {
         //prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe)
+
         //create recipe object
         state.recipe = new Recipe(id);
 
@@ -73,8 +77,10 @@ const controlRecipe = async () => {
             state.recipe.calcTime()
             state.recipe.calcServings()
             //render recipe
-            console.log(state.recipe)
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
+            console.log(error)
             alert('Error processing recipe!')
         }
     }
