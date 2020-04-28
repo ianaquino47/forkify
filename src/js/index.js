2d9e998fd97fc297d6596b404d9d4c01c4cb1891
iAquino47 <aquinoian@icloud.com>
2020-04-28 21:28:10 +0100

    persisting data using localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -130,10 +130,6 @@ elements.shopping.addEventListener('click', e => {
 
 //Like CONTROLLER
 
-//testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes())
-
 const controlLike = () => {
     //create a new likes IF there is none yet
     if (!state.likes) state.likes = new Likes();
@@ -165,6 +161,20 @@ const controlLike = () => {
 
     likesView.toggleLikeMenu(state.likes.getNumLikes())
 }
+
+//Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes;
+
+    //Restore likes
+    state.likes.readStorage();
+
+    //Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes())
+
+    //Reander the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+})
 
 //Handling Recipe button clicks
 elements.recipe.addEventListener('click', e => {
