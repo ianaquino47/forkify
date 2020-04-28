7a811d36e6c0398a9ffb766c2da5c7ac9ea720e6
iAquino47 <aquinoian@icloud.com>
2020-04-28 17:20:44 +0100

    shopping list now displays on the UI

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import Recipe from './models/Recipe'
 import List from './models/List'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
+import * as listView from './views/listView'
 import { elements, renderLoader, clearLoader } from './views/base'
 
 // Global state of the app
@@ -93,6 +94,17 @@ const controlRecipe = async () => {
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
 
+//List CONTROLLER
+const controlList = () => {
+    //create a new list IF there is none yet
+    if (!state.list) state.list = new List();
+
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
 //Handling Recipe button clicks
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -105,6 +117,8 @@ elements.recipe.addEventListener('click', e => {
         //Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe)
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
     console.log(state.recipe)
 });
